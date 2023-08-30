e8290484adce13e4e82a50bd8819525042d9acd1
root <root@ferdinandimeka>
2023-08-30 18:57:21 +0100

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@
 import os
 import re
 import logging
-import MySQLdb.connections
+import mysql.connector
 from typing import List
 
 
@@ -36,14 +36,14 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> MySQLdb.connections.Connection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Creates a connector to a database.
     """
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "root")
-    connection = MySQLdb.connections.Connection(
+    connection = mysql.connector.connect(
         host=db_host,
         port=3306,
         user=db_user,
