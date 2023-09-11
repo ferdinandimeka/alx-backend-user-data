0b859b30c478b82c67b131578e519317589775f4
root <root@ferdinandimeka>
2023-09-11 14:53:48 +0100

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,10 +13,12 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def _generate_uuid() -> str:
     """Generate UUID
     """
     return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -33,16 +35,18 @@ class Auth:
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        
+
     def valid_login(self, email: str, password: str) -> bool:
         """Validate login
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password
+                )
         except NoResultFound:
             return False
-        
+
     def create_session(self, email: str) -> str:
         """Create session
         """
